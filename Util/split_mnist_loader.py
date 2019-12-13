1a995df7eea1260a461705393db1d7fc5267a419
MohamedAbdelsalam9 <mohamedashraf9412@gmail.com>
2019-12-13 18:17:35 -0500

    Initial commit

---
+++ Util/split_mnist_loader.py
@@ -0,0 +1,75 @@
+import os, sys
+import numpy as np
+import torch
+from torchvision import datasets, transforms
+from sklearn.utils import shuffle
+
+# code from https://github.com/csm9493/UCL/blob/master/dataloaders/split_mnist.py
+def get_data(seed=0, fixed_order=False, pc_valid=0, datapath=".", tasknum = 5):
+    split_mnist_datapath = os.path.join(datapath, "binary_split_mnist")
+    if tasknum >5:
+        tasknum = 5
+    data = {}
+    taskcla = []
+    size = [1, 28, 28]
+
+    # Pre-load
+    # MNIST
+    mean = (0.1307,)
+    std = (0.3081,)
+    if not os.path.isdir(split_mnist_datapath):
+        os.makedirs(split_mnist_datapath)
+        dat = {}
+        dat['train'] = datasets.MNIST(datapath, train=True, download=True, transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(mean, std)]))
+        dat['test'] = datasets.MNIST(datapath, train=False, download=True, transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(mean, std)]))
+        for i in range(5):
+            data[i] = {}
+            data[i]['name'] = 'split_mnist-{:d}'.format(i)
+            data[i]['ncla'] = 2
+            data[i]['train'] = {'x': [], 'y': []}
+            data[i]['test'] = {'x': [], 'y': []}
+        for s in ['train', 'test']:
+            loader = torch.utils.data.DataLoader(dat[s], batch_size=1, shuffle=False)
+            for image, target in loader:
+                task_idx = target.numpy()[0] // 2
+                data[task_idx][s]['x'].append(image)
+                data[task_idx][s]['y'].append(target.numpy()[0 ] %2)
+
+        for i in range(5):
+            for s in ['train', 'test']:
+                data[i][s]['x'] = torch.stack(data[i][s]['x'])
+                data[i][s]['y'] = torch.LongTensor(np.array(data[i][s]['y'], dtype=int)).view(-1)
+                torch.save(data[i][s]['x']
+                           ,os.path.join(os.path.expanduser(split_mnist_datapath), 'data '+ str(i) + s + 'x.bin'))
+                torch.save(data[i][s]['y']
+                           ,os.path.join(os.path.expanduser(split_mnist_datapath), 'data '+ str(i) + s + 'y.bin'))
+    else:
+        # Load binary files
+        for i in range(5):
+            data[i] = dict.fromkeys(['name', 'ncla', 'train', 'test'])
+            data[i]['ncla'] = 2
+            data[i]['name'] = 'split_mnist-{:d}'.format(i)
+
+            # Load
+            for s in ['train', 'test']:
+                data[i][s] = {'x': [], 'y': []}
+                data[i][s]['x'] = torch.load \
+                    (os.path.join(os.path.expanduser(split_mnist_datapath), 'data '+ str(i) + s + 'x.bin'))
+                data[i][s]['y'] = torch.load \
+                    (os.path.join(os.path.expanduser(split_mnist_datapath), 'data '+ str(i) + s + 'y.bin'))
+
+    for t in range(tasknum):
+        data[t]['valid'] = {}
+        data[t]['valid']['x'] = data[t]['train']['x'].clone()
+        data[t]['valid']['y'] = data[t]['train']['y'].clone()
+
+    # Others
+    n = 0
+    for t in range(tasknum):
+        taskcla.append((t, data[t]['ncla']))
+        n += data[t]['ncla']
+    data['ncla'] = n
+
+    return data, taskcla, size
