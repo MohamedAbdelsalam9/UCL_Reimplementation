d4fceaf83396cace31ee62d02d7974484f023da0
amir9ume <amir9ume@gmail.com>
2019-12-13 20:38:26 -0500

    added gaussian sampler

--- Model/Bayes_Layers.py
+++ Model/Bayes_Layers.py
@@ -1,20 +1,42 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from torch.distributions import normal
 
 class BayesLinear(nn.Module):
     def __init__(self, in_shape, out_shape, ratio):
         super(BayesLinear).__init__()
         self.in_shape = in_shape
         self.out_shape = out_shape
+        
         self.weight_mu = nn.Parameter(torch.zeros((out_shape, in_shape)), requires_grad=True)
         self.weight_rho = nn.Parameter(torch.zeros((out_shape, 1)), requires_grad=True)
-
+        
+        
+        
     def forward(self, input, sample=False):
         device = input.device
         output = torch.zeros((self.out_shape)).to(device)
         return output
 
+class Gaussian()
+    def __init__(self,mu, rho):
+        
+        self.mu= mu
+        self.rho= rho
+        #noise is always in between 0 and 1
+        self.epsilon_noise = Normal(torch.tensor([0.0]), torch.tensor([1.0]))
+        
+    
+    def sigma(self,):
+        sigma= torch.log1p(torch.exp(self.rho))
+        return sigma
+        
+    def sample(self, sigma,):
+        
+        return self.mu + sigma * self.epsilon_noise.sample(torch.shape(self.mu))
+     
+    
 
 class BayesNet(nn.Module):
     def __init__(self, in_shape, out_shape, num_hidden_layers=1, hidden_sizes = [128], ratio=0.5):
